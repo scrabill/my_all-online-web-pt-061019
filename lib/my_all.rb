fd8a9c8df0581c45f9cb601bd4c963bd8f9e2e24
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-13 19:41:04 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  block_return_values = []
+  block_return_values = [] # Set up an empty array, to store the return of the block results
 
   while i < collection.length
     block_return_values << yield(collection[i])
@@ -12,6 +12,6 @@ def my_all?(collection)
   if block_return_values.include?(false) # If any of the values passed into the block are false...
     false # Then return false
   else
-    true # Otherwise, return true (all values are tr)
-  end  
+    true # Otherwise, return true
+  end
 end
