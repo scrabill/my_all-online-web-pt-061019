a71a358180db83d46e3be3585d29e180e7ab09dc
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-13 19:40:00 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,4 +8,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i = i + 1
   end
+
+  if block_return_values.include?(false) # If any of the values passed into the block are false...
+    false # Then return false
+  else
+    true # Otherwise, return true (all values are tr)
+  end  
 end
